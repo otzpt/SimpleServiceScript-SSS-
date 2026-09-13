75d5a53ff9205bcc012a37dd0f6c992b7367bd74
otzpt <mrjs10375@gmail.com>
2026-09-13 20:36:43 +0100

    more work done on windows version(this is the most polished version will add the new things to the others)

--- win.py
+++ win.py
@@ -22,4 +22,56 @@ def activateProcess():
     else:
         print("service is alredy running")
 
-activateProcess()
+def deactivateService():
+    service = input("what service do you want to stop: ")
+    output = subprocess.run(["sc", "query", service], capture_output=True, text = True)
+    status = output.stdout.strip()
+
+    if "RUNNING" in status:
+        subprocess.run(["sc", "stop", service])
+        # recheck
+        output = subprocess.run(["sc", "query", service], capture_output=True, text = True)
+        status = output.stdout.strip()
+        if "RUNNING" in status:
+            print("service failed to stop")
+        elif "STOPPED" in status:
+            print("service stopped")
+        else:
+            print("IDK")
+
+def serviceRestart():
+    service = input("what service do you want to restart: ")
+    output = subprocess.run(["sc", "query", service], capture_output=True, text = True)
+    status = output.stdout.strip()
+    if "RUNNING" in status:
+        subprocess.run(["sc", "stop", service])
+        print(f"{service} is stopping...")
+        time.sleep(5)
+        output = subprocess.run(["sc", "query", service], capture_output=True, text = True)
+        status = output.stdout.strip()
+        if "RUNNING" in status:
+            print("restart failed")
+        subprocess.run(["sc", "start", service])
+        print(f"{service} is starting...")
+        output = subprocess.run(["sc", "query", service], capture_output=True, text = True)
+        status = output.stdout.strip()
+        if "STOPPED" in status:
+            print("service failed start")
+    elif "STOPPED" in status:
+        subprocess.run(["sc", "start", service])
+        print(f"{service} is starting...")
+        time.sleep(5)
+        output = subprocess.run(["sc", "query", service], capture_output=True, text = True)
+        status = output.stdout.strip()
+        if "RUNNING" in status:
+            print("restart failed")
+        subprocess.run(["sc", "stop", service])
+        print(f"{service} is stopping...")
+        output = subprocess.run(["sc", "query", service], capture_output=True, text = True)
+        status = output.stdout.strip()
+        if "RUNNING" in status:
+            print("service failed to stop")
+        print("if you wanted the service to start use start in menu options \n (restart return's service to state that it was before restart)")
+
+
+print("if you wanted the service to start use start in menu options \n (restart return's service to state that it was before restart)")
